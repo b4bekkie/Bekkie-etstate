efdc4949bef31d8ab0e5bc79253c3a352c2e7fc4
BEKKIE <iambekkie99@gmail.com>
2023-10-22 17:46:44 +0500

    connected To DB MongoDB atlas

--- Real-estate/api/index.js
+++ Real-estate/api/index.js
@@ -1,5 +1,16 @@
 import express from 'express'
 const app = express();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config()
+
+mongoose.connect(process.env.MONGOSE)
+.then( ()=>{
+    console.log('Connected to DataBase')
+})
+.catch((e)=>{
+ console.log(e)
+})
 
 
 app.listen(3000,()=>{
