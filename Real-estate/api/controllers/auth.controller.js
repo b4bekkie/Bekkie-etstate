50fcba64e4da327513f013dbafc8f2652a472484
BEKKIE <iambekkie99@gmail.com>
2023-10-23 01:58:55 +0500

    created signup  api route and hashed pass feature

--- Real-estate/api/controllers/auth.controller.js
+++ Real-estate/api/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 import User from "../models/user.model.js";
+import bcryptjs from 'bcryptjs'
 
 export const signup =  ( async (req,res)=>{
 
     const {username,email,password} = req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10)
 
-    const newUser = new User({username,email,password});
+    const newUser = new User({username,email,password : hashedPassword});
    await newUser.save();
    res.status(404).json('user created successfully at db')
 
