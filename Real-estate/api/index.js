3fca31f2315f483f9b42f4c980cc05bce292cc77
BEKKIE <iambekkie99@gmail.com>
2023-11-10 01:25:55 +0500

    created user api api added

--- Real-estate/api/index.js
+++ Real-estate/api/index.js
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser';
 dotenv.config()
 
 
@@ -17,6 +18,7 @@ mongoose.connect(process.env.MONGOSE)
 })
 
 app.use(express.json())
+app.use(cookieParser());
 
 
 app.listen(3000,()=>{
--- Real-estate/api/routes/user.route.js
+++ Real-estate/api/routes/user.route.js
@@ -1,7 +1,11 @@
-import express from "express";
-import { test } from "../controllers/user.controller.js";
+import express from 'express';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
+
 const router = express.Router();
 
-router.get('/test',test)
-export default router;
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
 
+export default router;
