530428c39699fe2d63b68602e54c8f31a9a02571
BEKKIE <iambekkie99@gmail.com>
2023-11-11 00:44:05 +0500

    create listing ui added

--- Real-estate/client/src/App.jsx
+++ Real-estate/client/src/App.jsx
@@ -7,6 +7,7 @@ import SignOut from "./pages/SignUp"
 import SignUp from "./pages/SignUp"
 import Header from "./components/Header"
 import PrivateRoute from "./components/PrivateRoute"
+import CreateListing from "./pages/CreateListing"
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
       <Route path="/about" element={<About/>} />
       <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing/>} />
         </Route>
       <Route path="/sign-in" element={<SignIn/>} />
       <Route path="/sign-up" element={<SignUp/>} />
--- Real-estate/client/src/pages/Profile.jsx
+++ Real-estate/client/src/pages/Profile.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
 import { useRef, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import {
   getDownloadURL,
   getStorage,
@@ -178,6 +179,9 @@ export default function Profile() {
         >
           {loading ? 'Loading...' : 'Update'}
         </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className='flex justify-between mt-5'>
       <span
