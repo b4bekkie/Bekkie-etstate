90129434c5d603391e53eaee0222fe88e00ef169
BEKKIE <iambekkie99@gmail.com>
2023-11-10 18:48:53 +0500

    complete delete user funs added

--- Real-estate/client/src/redux/user/userSlice.js
+++ Real-estate/client/src/redux/user/userSlice.js
@@ -33,10 +33,24 @@ const userSlice = createSlice({
           updateUserFailure: (state, action) => {
             state.error = action.payload;
             state.loading = false;
-          }
+          },
+          deleteUserStart: (state) => {
+            state.loading = true;
+          },
+          deleteUserSuccess: (state) => {
+            state.currentUser = null;
+            state.loading = false;
+            state.error = null;
+          },
+          deleteUserFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+          },
     }
 });
 
-export const { signInStart, signInSuccess, signInFailure ,updateUserStart ,updateUserSuccess,updateUserFailure } = userSlice.actions;
+export const { signInStart, signInSuccess, signInFailure ,updateUserStart ,updateUserSuccess,updateUserFailure,deleteUserFailure,
+    deleteUserSuccess,
+    deleteUserStart, } = userSlice.actions;
 
 export default userSlice.reducer
