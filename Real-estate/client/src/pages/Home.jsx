312745fcc41ecd16b28cb25c13dee5742e684856
Bilal Khan <97617809+b4bekkie@users.noreply.github.com>
2023-11-19 04:05:49 +0500

    Update Home.jsx
    Home bug fixed

--- Real-estate/client/src/pages/Home.jsx
+++ Real-estate/client/src/pages/Home.jsx
@@ -52,7 +52,7 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto  text-center'>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
           Find your next <span className='text-slate-500'>perfect</span>
           <br />
