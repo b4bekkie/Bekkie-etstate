2cb6bf77b12d0ea7e250ea75b145771b7a37fea2
BEKKIE <iambekkie99@gmail.com>
2023-11-18 19:40:47 +0500

    try

--- Real-estate/client/vite.config.js
+++ Real-estate/client/vite.config.js
@@ -5,14 +5,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://bekkie-etstate.vercel.app',
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://bekkie-etstate.vercel.app',
+  //       secure: false,
+  //     },
+  //   },
+  // },
   plugins: [react()],
   
   
